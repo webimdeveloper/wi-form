ea66278a6ffaba8f8b4c0af87221d91c2cb5fb68
Olha Harlamova <wi.kharlamova@gmail.com>
2025-12-09 13:09:46 +0100

    Initial plugin skeleton

---
+++ wp-form-calculator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WP Form Calculator
+ * Description: WP Form Calculator
+ * Version: 0.1.0
+ * Author: Serhii Hryshyn
+ * Text Domain: wiform
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Your plugin logic will go here.
